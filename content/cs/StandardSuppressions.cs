829182a5590703b36a134073ea79766dc4727941
Adrian Mos <adimosh@outlook.com>
2021-07-12 22:17:59 +0300

    Fixed file header issue.

--- content/cs/StandardSuppressions.cs
+++ content/cs/StandardSuppressions.cs
@@ -1,4 +1,4 @@
-// <copyright file="HeapAllocationAnalyzerSuppressions.cs" company="Adrian Mos">
+// <copyright file="StandardSuppressions.cs" company="Adrian Mos">
 // Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
 // </copyright>
 
