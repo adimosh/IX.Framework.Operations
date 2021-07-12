645cc466a529232c0c993c1580475f541ad7523f
Adrian Mos <adimosh@outlook.com>
2021-07-12 22:16:30 +0300

    Improved and renamed suppressions file.

--- content/cs/StandardSuppressions.cs
+++ content/cs/StandardSuppressions.cs
@@ -4,6 +4,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+// Heap allocations analyzer will show just about every allocation possible as a warning
 [assembly: SuppressMessage("Performance", "HAA0504:Implicit new array creation allocation", Justification = "Explicit array creation is usually an intended phenomenon, for now let's ignore.", Scope = "module")]
 [assembly: SuppressMessage("Performance", "HAA0502:Explicit new reference type allocation", Justification = "Explicit new allocations will only be optimizable in the future.", Scope = "module")]
 [assembly: SuppressMessage("Performance", "HAA0101:Array allocation for params parameter", Justification = "So params allocates an empty array anytime. So what? It's not like I have a choice.", Scope = "module")]
@@ -11,8 +12,13 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Performance", "HAA0505:Initializer reference type allocation", Justification = "Explicit collection creation by initializer is usually an intended phenomenon, for now let's ignore.", Scope = "module")]
 [assembly: SuppressMessage("Performance", "HAA0503:Explicit new anonymous object allocation", Justification = "LINQ works that way, it is rare enough to be of little impact anyway.", Scope = "module")]
 
+// These are rather silly
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1413:UseTrailingCommasInMultiLineInitializers", Justification = "Not observed.", Scope = "module")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1407:Arithmetic expressions should declare precedence", Justification = "Not observed.", Scope = "module")]
 
+// We don't agree with the StyleCop rules that regions are useless
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1123:Do not place regions within elements", Justification = "Not observed. Regions make complex code breathable.", Scope = "module")]
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "Not observed. Regions make complex code breathable.", Scope = "module")]
+
+// This is as of yet impossible to configure
+[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1502:Element should not be on a single line", Justification = "We should have it on one line if it makes our code less cluttered.", Scope = "module")]
