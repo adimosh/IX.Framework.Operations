c6ae5c4c2ceda2986a01b800244676cc862a5bb3
Adrian Mos <adimosh@outlook.com>
2022-07-08 16:01:56 +0300

    Removed StyleCop as it's mostly a useless extra check after editorconfig and R#

--- content/cs/StandardSuppressions.cs
+++ content/cs/StandardSuppressions.cs
@@ -11,16 +11,3 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Performance", "HAA0501:Explicit new array type allocation", Justification = "Explicit array creation will be refactored at a later time, wherever necessary.", Scope = "module")]
 [assembly: SuppressMessage("Performance", "HAA0505:Initializer reference type allocation", Justification = "Explicit collection creation by initializer is usually an intended phenomenon, for now let's ignore.", Scope = "module")]
 [assembly: SuppressMessage("Performance", "HAA0503:Explicit new anonymous object allocation", Justification = "LINQ works that way, it is rare enough to be of little impact anyway.", Scope = "module")]
-
-// These are rather silly
-[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1413:UseTrailingCommasInMultiLineInitializers", Justification = "Not observed.", Scope = "module")]
-[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1407:Arithmetic expressions should declare precedence", Justification = "Not observed.", Scope = "module")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1601:Partial elements should be documented", Justification = "This conflicts with how XML documentation works.")]
-
-// We don't agree with the StyleCop rules that regions are useless
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1123:Do not place regions within elements", Justification = "Not observed. Regions make complex code breathable.", Scope = "module")]
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "Not observed. Regions make complex code breathable.", Scope = "module")]
-[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1207:ProtectedMustComeBeforeInternal", Justification = "Not observed. There is no point in making a rule that changes what everybody else has been taught to use.", Scope="module")]
-
-// This is as of yet impossible to configure
-[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1502:Element should not be on a single line", Justification = "We should have it on one line if it makes our code less cluttered.", Scope = "module")]
